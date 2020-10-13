40260b07b15c9ead7d681350929a2fee852f19c4
austinkelsay <austinkelsay@yahoo.com>
2020-10-13 16:54:51 -0500

    DataDisplay setup to recieve HTML

--- src/components/dashboard/DataDisplay.js
+++ src/components/dashboard/DataDisplay.js
@@ -1,10 +1,14 @@
 import React from "react"
+import parse from 'html-react-parser';
+import { useSelector } from "react-redux";
 
 const DataDisplay = () => {
-
+    const data = useSelector(state => state.active_data_type)
     return (
         <div>
-            test
+            {
+                parse(data)
+            }
         </div>
     )
 }
