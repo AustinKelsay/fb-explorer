19b9281a84d5cd74073a7eeab9bd8cb14afc7ffe
austinkelsay <austinkelsay@yahoo.com>
2020-10-20 17:15:21 -0500

    Still working on parsing

--- src/components/dashboard/DataDisplay.js
+++ src/components/dashboard/DataDisplay.js
@@ -1,15 +1,20 @@
 import React from "react"
 import parse from 'html-react-parser';
 import { useSelector } from "react-redux";
-import test from "./messenger.html"
+
 
 const DataDisplay = () => {
     const data = useSelector(state => state.active_data_type)
     const activeData = Object.keys(data)[0]
+    console.log(data[activeData])
     return (
         <div>
             {
-                // parse(`${activeData}`)
+                Object.entries(data[activeData]).map((value, key) => {
+                    console.log(key)
+                    console.log(value[1])
+                    parse(`${value[1]}`)
+                })
             }
         </div>
     )
