ac066372e51160861a53a34f57794c2b50d3790f
austinkelsay <austinkelsay@yahoo.com>
2020-11-20 14:42:52 -0600

    Got loading ring working

--- src/index.js
+++ src/index.js
@@ -3,24 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
-import {Route} from "react-router-dom"
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from "react-redux";
 import { Reducer } from "./store/Reducer";
-import {persistStore, persistReducer} from "redux-persist"
-import {PersistGate} from "redux-persist/integration/react"
-import storage from "redux-persist/lib/storage/session"
 import thunk from "redux-thunk";
-import logger from 'redux-logger'
 
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// }
-
-// const persistedReducer = persistReducer(persistConfig, Reducer)
 const store = createStore(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-const persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={store}>
