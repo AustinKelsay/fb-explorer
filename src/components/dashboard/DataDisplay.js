5f57bac08c113072b135d4ea87b29bbfbc41ff4b
austinkelsay <austinkelsay@yahoo.com>
2020-11-25 09:07:22 -0600

    Blobs arent working in the src attrib

--- src/components/dashboard/DataDisplay.js
+++ src/components/dashboard/DataDisplay.js
@@ -3,10 +3,31 @@ import parse from "html-react-parser"
 import {useSelector} from "react-redux"
 
 const DataDisplay = (props) => {
-    const userData = useSelector(state => state.userData)
-    console.log(userData)
-    console.log(props.data)
-    return parse(props.data)
+    const media = useSelector(state => state.media)
+
+    const options = {
+        replace: (domNode) => {
+          if (!domNode.attribs) {
+            return
+          }
+          if (domNode.attribs.src) {
+              const mediaItems = domNode.attribs.src.split('/')
+              const mediaItem = mediaItems.pop()
+              const mediaName = mediaItem.split('.')
+              console.log(`blob:https://localhost:3000/${mediaName[0]}`)
+
+            //   return(
+            //       <img src={`blob:https://localhost:3000/${mediaName[0]}`} className={domNode.attribs.class} />
+            //   )
+            }
+          }
+        }
+  
+      
+  
+      return (
+        parse(props.data, options)
+      )
 }
 
 export default DataDisplay
