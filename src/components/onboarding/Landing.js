c3ac7bfd9d477ba861bfa696deb16e853d1d12cf
austinkelsay <austinkelsay@yahoo.com>
2020-12-10 14:53:07 -0600

    Started on landing page and added video

---
+++ src/components/onboarding/Landing.js
@@ -0,0 +1,18 @@
+import React from "react"
+import categoryScroll from "./media/category-scroll.mp4"
+
+const Landing = () => {
+
+    return (
+        <div className="landing">
+            <h1 className="logo" >fbexplorer</h1>
+
+            <div className="description">
+                <p>fbexplorer lets you reclaim, explore, and search all of your Facebook data</p>
+                <video src={categoryScroll} className="description-video" preload="auto" muted autoplay loop />
+            </div>
+        </div>
+    )
+}
+
+export default Landing
