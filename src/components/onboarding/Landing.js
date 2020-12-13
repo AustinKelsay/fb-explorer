79a735b994f92ab7174de20a168b6f9159a47ba0
austinkelsay <austinkelsay@yahoo.com>
2020-12-13 17:22:08 -0600

    Next iteration of landing but its still not quite there

--- src/components/onboarding/Landing.js
+++ src/components/onboarding/Landing.js
@@ -1,5 +1,6 @@
 import React from "react"
 import categoryScroll from "./media/category-scroll.mp4"
+import {FaHandPointRight} from "react-icons/fa"
 
 const Landing = () => {
 
@@ -11,15 +12,19 @@ const Landing = () => {
                 <p>fbexplorer lets you reclaim, explore, and search all of your Facebook data</p>
                 <video src={categoryScroll} className="description-video" preload="auto" muted autoplay='true' loop />
                 <h3 className="step">Step 1: Log into Facebook</h3>
+                <div  id="fb-login"
+                    className="fb-login-button" 
+                    scope="public_profile"
+                    data-width="" 
+                    data-size="large" 
+                    data-button-type="login_with" 
+                    data-auto-logout-link="false" 
+                    data-use-continue-as="false">
+                </div>
                 <div className="button-container">
-                    <div  id="fb-login"
-                        className="fb-login-button" 
-                        scope="public_profile"
-                        data-width="" 
-                        data-size="large" 
-                        data-button-type="login_with" 
-                        data-auto-logout-link="false" 
-                        data-use-continue-as="false">
+                    <div className="skip-container">
+                        <FaHandPointRight className='icon-hand-point' />
+                        <a href="/drop" className="icon-text">Already have your data</a>
                     </div>
                     <button className='next'>Next</button>
                 </div>
